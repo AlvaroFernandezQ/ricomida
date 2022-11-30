e14de6ce21d7c6b68bb68f4d2d3fa338a304e1c5
Alvaro Fernandez <alvaro.fernandezqui@mail.com>
2022-11-30 20:07:09 -0300

    Inicio de ricomida

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $(".red-menu").dblclick(function(){
+
+        $(this).css({
+
+            "color":"red"
+        })
+    })
+
+    
+    
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+})
